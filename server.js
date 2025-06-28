9d2271d9db8d70de66a1cb2db662d668eeb4dd0d
Subhashini <subhashinikumar2004@gmail.com>
2025-06-28 20:11:06 +0530

    connected to MongoDB

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const connectDB = require("./config/db");
 
 const app = express();
 
@@ -14,10 +15,19 @@ app.use(
     })
 );
 
+connectDB()
+
+
 //Middleware
 app.use(express.json());
 
 //Routes
+// app.use("/api/auth", authRoutes);
+// app.use("/api/sessions", sessionRoutes);
+// app.use("/api/questions", questionRoutes);
+
+// app.use("/api/ai/generate-questions", ProcessingInstruction, generateInterviewQuestions);
+// app.use("/api/ai/generate-explanation", ProcessingInstruction, generateConceptExplanation);
 
 
 //Serve uploads folder
