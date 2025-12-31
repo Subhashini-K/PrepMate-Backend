5adce31928177467e9ba308eddd824bb90982c7b
Subhashini <subhashinikumar2004@gmail.com>
2025-12-31 11:17:44 +0530

    Resolved cors error

--- server.js
+++ server.js
@@ -16,7 +16,12 @@ app.use(express.json());
 
 //Middleware to handle CORS
 app.use(
-    cors()
+    cors({
+        origin: ["https://prepmate.subhashini.site", "http://localhost:5173", "http://localhost:3000"],
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"]
+    })
 );
 
 //Routes
@@ -24,8 +29,8 @@ app.use("/api/auth", authRoutes);
 app.use("/api/sessions", sessionRoutes);
 app.use("/api/questions", questionRoutes);
 
-app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
-app.use("/api/ai/generate-explanation", protect , generateConceptExplanation);
+app.post("/api/ai/generate-questions", protect, generateInterviewQuestions);
+app.post("/api/ai/generate-explanation", protect , generateConceptExplanation);
 
 
 //Serve uploads folder
